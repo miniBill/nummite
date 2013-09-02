3358c5e0c8036e4cf3ca7846ae6115df7e84176c
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2013-09-02 11:23:25 +0200

    Cleanup

--- Forms/OptionPanes/Objects.Designer.cs
+++ Forms/OptionPanes/Objects.Designer.cs
@@ -27,106 +27,85 @@ namespace DiagramDrawer.Forms.OptionPanes {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		void InitializeComponent() {
-			tableLayoutPanel1 = new TableLayoutPanel();
-			label1 = new Label();
-			numericUpDown1 = new NumericUpDown();
-			label2 = new Label();
-			numericUpDown2 = new NumericUpDown();
-			tableLayoutPanel1.SuspendLayout();
-			(numericUpDown1).BeginInit();
-			(numericUpDown2).BeginInit();
-			SuspendLayout();
-			// 
-			// tableLayoutPanel1
-			// 
-			tableLayoutPanel1.ColumnCount = 2;
-			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
-			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-			tableLayoutPanel1.Controls.Add(label1, 0, 0);
-			tableLayoutPanel1.Controls.Add(label2, 0, 1);
-			tableLayoutPanel1.Controls.Add(numericUpDown1, 1, 0);
-			tableLayoutPanel1.Controls.Add(numericUpDown2, 1, 1);
-			tableLayoutPanel1.Dock = DockStyle.Fill;
-			tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-			tableLayoutPanel1.Name = "tableLayoutPanel1";
-			tableLayoutPanel1.RowCount = 3;
-			tableLayoutPanel1.RowStyles.Add(new RowStyle());
-			tableLayoutPanel1.RowStyles.Add(new RowStyle());
-			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-			tableLayoutPanel1.Size = new System.Drawing.Size(277, 62);
-			tableLayoutPanel1.TabIndex = 0;
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.SuspendLayout();
 			// 
 			// label1
 			// 
-			label1.Anchor = AnchorStyles.Left;
-			label1.AutoSize = true;
-			label1.Location = new System.Drawing.Point(3, 6);
-			label1.Name = "label1";
-			label1.Size = new System.Drawing.Size(91, 13);
-			label1.TabIndex = 0;
-			label1.Text = "Larghezza minima";
-			// 
-			// numericUpDown1
-			// 
-			numericUpDown1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-			numericUpDown1.Increment = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
-			numericUpDown1.Location = new System.Drawing.Point(100, 3);
-			numericUpDown1.Maximum = new decimal(new int[] {
-            200,
-            0,
-            0,
-            0});
-			numericUpDown1.Minimum = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
-			numericUpDown1.Name = "numericUpDown1";
-			numericUpDown1.Size = new System.Drawing.Size(174, 20);
-			numericUpDown1.TabIndex = 2;
-			numericUpDown1.Value = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 5);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(91, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Larghezza minima";
 			// 
 			// label2
 			// 
-			label2.Anchor = AnchorStyles.Left;
-			label2.AutoSize = true;
-			label2.Location = new System.Drawing.Point(3, 32);
-			label2.Name = "label2";
-			label2.Size = new System.Drawing.Size(76, 13);
-			label2.TabIndex = 1;
-			label2.Text = "Altezza minima";
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 31);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(76, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Altezza minima";
 			// 
-			// numericUpDown2
+			// numericUpDown1
 			// 
-			numericUpDown2.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-			numericUpDown2.Increment = new decimal(new int[] {
+			this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.numericUpDown1.Increment = new decimal(new int[] {
             10,
             0,
             0,
             0});
-			numericUpDown2.Location = new System.Drawing.Point(100, 29);
-			numericUpDown2.Maximum = new decimal(new int[] {
+			this.numericUpDown1.Location = new System.Drawing.Point(100, 3);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
             200,
             0,
             0,
             0});
-			numericUpDown2.Minimum = new decimal(new int[] {
+			this.numericUpDown1.Minimum = new decimal(new int[] {
             10,
             0,
             0,
             0});
-			numericUpDown2.Name = "numericUpDown2";
-			numericUpDown2.Size = new System.Drawing.Size(174, 20);
-			numericUpDown2.TabIndex = 3;
-			numericUpDown2.Value = new decimal(new int[] {
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(578, 20);
+			this.numericUpDown1.TabIndex = 2;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.numericUpDown2.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Location = new System.Drawing.Point(100, 29);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(578, 20);
+			this.numericUpDown2.TabIndex = 3;
+			this.numericUpDown2.Value = new decimal(new int[] {
             10,
             0,
             0,
@@ -134,22 +113,23 @@ namespace DiagramDrawer.Forms.OptionPanes {
 			// 
 			// Objects
 			// 
-			AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			Controls.Add(tableLayoutPanel1);
-			Name = "Objects";
-			Size = new System.Drawing.Size(277, 62);
-			tableLayoutPanel1.ResumeLayout(false);
-			tableLayoutPanel1.PerformLayout();
-			numericUpDown1.EndInit();
-			numericUpDown2.EndInit();
-			ResumeLayout (false);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.numericUpDown2);
+			this.Name = "Objects";
+			this.Size = new System.Drawing.Size(681, 67);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		TableLayoutPanel tableLayoutPanel1;
 		Label label1;
 		Label label2;
 		NumericUpDown numericUpDown1;
